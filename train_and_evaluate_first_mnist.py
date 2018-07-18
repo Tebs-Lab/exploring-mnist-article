f0198e72635e2f384718b18af49c1c4caa039fb7
Tyler Bettilyon <teb@tebs-lab.com>
2018-07-18 14:01:08 -0700

    Update train_and_evaluate_first_mnist.py

--- train_and_evaluate_first_mnist.py
+++ train_and_evaluate_first_mnist.py
@@ -1,19 +1,14 @@
-# sgd is short for stochastic gradient descent
 model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(x_train, y_train, batch_size=128, epochs=5, verbose=False, validation_split=.1)
+loss, accuracy  = model.evaluate(x_test, y_test, verbose=False)
 
-# Note the batch_size and epochs
-history = model.fit(x_train,
-                    y_train,
-                    batch_size=128,
-                    epochs=5,
-                    verbose=True,
-                    validation_split=.1)
-
-# The Keras library reports the value of the value of the loss function
-# and the accuracy metric (because we requested it on line 2)
-loss, accuracy  = model.evaluate(x_test,
-                                 y_test,
-                                 verbose=False)
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['training', 'validation'], loc='best')
+plt.show()
 
 print(f'Test loss: {loss:.3}')
 print(f'Test accuracy: {accuracy:.3}')
