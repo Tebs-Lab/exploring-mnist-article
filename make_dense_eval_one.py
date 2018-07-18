fa509347105478fe6c9636ff60f9de12129585e2
Tyler Bettilyon <teb@tebs-lab.com>
2018-07-18 14:02:03 -0700

    Update make_dense_eval_one.py

--- make_dense_eval_one.py
+++ make_dense_eval_one.py
@@ -9,11 +9,19 @@ def create_dense(layer_sizes):
 
     return model
 
-def evaluate(model, x_train, y_train, x_test, y_test):
+def evaluate(model, batch_size=128, epochs=5):
     model.summary()
     model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train, y_train, batch_size=128, epochs=5, validation_split=.1)
-    loss, accuracy  = model.evaluate(x_test, y_test, verbose=True)
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=.1, verbose=False)
+    loss, accuracy  = model.evaluate(x_test, y_test, verbose=False)
+    
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['training', 'validation'], loc='best')
+    plt.show()
 
     print()
     print(f'Test loss: {loss:.3}')
